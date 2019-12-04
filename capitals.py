7bd093d864e534d7bc704a1d06994e934a810dc7
Roberto <vlad.drahnea@gmail.com>
2019-12-04 15:47:02 +0200

    saparate function for letters counter

--- capitals.py
+++ capitals.py
@@ -1,15 +1,23 @@
-def letters_count():
-    print('Please write your text')
-    input_text = input()
-    count_lower = 0
+def count_letters(string):
     count_upper = 0
+    count_lower = 0
 
-    for i in input_text:
+    for i in string:
         if (i.islower()):
             count_lower = count_lower + 1 
         elif (i.isupper()):
             count_upper = count_upper + 1
-    print('There are {} uppercase letters'.format(count_upper))
-    print('There are {} lowercase letters'.format.(count_lower))
 
-letters_count()
+    return (count_upper, count_lower)
+
+def main():
+    print('Please write your text')
+    input_text = input()
+
+    results = count_letters(input_text)
+    
+    print('There are {} uppercase letters'.format(results[0]))
+    print('There are {} lowercase letters'.format(results[1]))
+
+if __name__ == "__main__":
+    main()
